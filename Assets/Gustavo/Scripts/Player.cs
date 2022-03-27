5877d7f049d38a6595c553a41f3c5382fa6dc7f9
O Gustavo <avoguga@gmail.com>
2022-03-27 14:33:38 -0300

    uploading abeia project

---
+++ Assets/Gustavo/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(horizontal, vertical, 0) * (speed * Time.deltaTime));
+    }
+}
---
+++ Assets/Gustavo/Scripts/Room.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public float Width;
+    public float Height;
+    public float X;
+    public float Y;
+
+    void Start()
+    {
+        if(RoomController.instance == null)
+        {
+            Debug.Log("U pressed play in the wrong scene!");
+            return;
+        }
+
+        RoomController.instance.RegisterRoom(this);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, new Vector3(Width, Height, 0));
+    }
+
+    public Vector3 GetRoomCentre()
+    {
+        return new Vector3(X * Width, Y * Height);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            RoomController.instance.OnPlayerEnterRoom(this);
+        }
+    }
+  
+}
